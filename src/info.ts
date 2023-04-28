e4bafd05a097321b1c21ea0362b2ecde18c781aa
Kishan Bagaria <1093313+KishanBagaria@users.noreply.github.com>
2023-04-28 12:09:13 -0700

    PlatformInfo.loginMode

--- src/info.ts
+++ src/info.ts
@@ -8,7 +8,7 @@ const info: PlatformInfo = {
   version: '1.0.0',
   displayName: 'ChatGPT',
   icon,
-  loginMode: 'browser',
+  loginMode: ['browser', 'browser-extension'],
   browserLogin: {
     url: 'https://chat.openai.com/',
     runJSOnLaunch: CLOSE_ON_AUTHENTICATED_JS,
